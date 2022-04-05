037ff4dc1b17cc5c1290811d2ab4a4da5ba7c0d0
wesleysoeliman <wesley.soeliman@gmail.com>
2022-04-05 12:13:32 +0200

    fixed nav styling

--- PLAII/header.php
+++ PLAII/header.php
@@ -1,5 +1,8 @@
 <nav>
+  <link rel="stylesheet" href="../css/main.css">
+  <div class="header">
   <a href="index.php">Homepagina</a>
   <a href="contact.php">Contactpagina</a>
   <a href="uniquesellingpoints.php">Unique selling points</a>
+</div>
 </nav>
